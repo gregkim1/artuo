d9fcc3df3e3d705f142317c1718989aba93f613c
Raghu Betina <raghu@firstdraft.com>
2018-03-07 02:38:31 +0000

    Whitelist columns in Tagging for ActiveAdmin

--- app/admin/taggings.rb
+++ app/admin/taggings.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Tagging do
+
+ permit_params :artwork_id, :tag_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
