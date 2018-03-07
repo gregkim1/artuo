8512ae29708d878cbad3b52dfa791b833f7a3b05
Raghu Betina <raghu@firstdraft.com>
2018-03-07 02:38:25 +0000

    Whitelist columns in Artwork for ActiveAdmin

--- app/admin/artworks.rb
+++ app/admin/artworks.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Artwork do
+
+ permit_params :caption, :image_url, :tag
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
