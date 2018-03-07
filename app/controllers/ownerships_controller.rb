8f18993f486f1048ec2425333dbcc3bc1f31568c
Raghu Betina <raghu@firstdraft.com>
2018-03-07 02:38:42 +0000

    Only ownership user can modify ownership

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -1,4 +1,14 @@
 class OwnershipsController < ApplicationController
+  before_action :current_user_must_be_ownership_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_ownership_user
+    ownership = Ownership.find(params[:id])
+
+    unless current_user == ownership.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @ownerships = Ownership.all
 
